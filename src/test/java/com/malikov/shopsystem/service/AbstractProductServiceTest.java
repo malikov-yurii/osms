7aeea07d1672248329da0b183728f9b5d5e0fb12
Malikov <malikov.yurii.alex@gmail.com>
2017-01-04 21:59:26 +0200

    github bug fixed

---
+++ src/test/java/com/malikov/shopsystem/service/AbstractProductServiceTest.java
@@ -0,0 +1,70 @@
+package com.malikov.shopsystem.service;
+
+import com.malikov.shopsystem.ProductTestData;
+import com.malikov.shopsystem.model.Product;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static com.malikov.shopsystem.ProductCategoryTestData.CATEGORY_KLEI;
+import static com.malikov.shopsystem.ProductCategoryTestData.CATEGORY_POTAL_I_ZOLOTO;
+import static com.malikov.shopsystem.ProductTestData.*;
+
+public abstract class AbstractProductServiceTest extends AbstractServiceTest {
+
+    @Autowired
+    protected ProductService service;
+
+    @Test
+    public void testSave() throws Exception {
+        Product newProduct = new Product("newProductName", 100f, 111, CATEGORY_KLEI);
+        Product created = service.save(newProduct);
+        newProduct.setId(created.getId());
+        ProductTestData.MATCHER.assertCollectionEquals(
+                Arrays.asList(POTAL_KITAJ, POTAL_NAZIONALE, SHELLAC_MANETTI, FERRARIO_ROZOVYJ, newProduct),
+                service.getAll());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        Product updated = new Product(POTAL_KITAJ);
+        updated.setName("Kitaj_upd");
+        service.update(updated);
+        ProductTestData.MATCHER.assertEquals(updated, service.get(POTAL_KITAJ.getId()));
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        Product product = service.get(POTAL_NAZIONALE.getId());
+        ProductTestData.MATCHER.assertEquals(POTAL_NAZIONALE, product);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        Collection<Product> all = service.getAll();
+        ProductTestData.MATCHER.assertCollectionEquals(Arrays.asList(POTAL_KITAJ, POTAL_NAZIONALE,
+                SHELLAC_MANETTI, FERRARIO_ROZOVYJ), all);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        service.delete(POTAL_NAZIONALE.getId());
+        ProductTestData.MATCHER.assertCollectionEquals(Arrays.asList(
+                POTAL_KITAJ, SHELLAC_MANETTI, FERRARIO_ROZOVYJ), service.getAll());
+    }
+
+    @Test
+    public void testGetByCategoryId() throws Exception {
+        Collection<Product> productsByCategoryId = service.getByCategoryId(CATEGORY_POTAL_I_ZOLOTO.getId());
+        ProductTestData.MATCHER.assertCollectionEquals(Arrays.asList(POTAL_NAZIONALE, POTAL_KITAJ), productsByCategoryId);
+    }
+
+    @Test
+    public void testGetAllQuantityLessThan() throws Exception {
+        Collection<Product> productsWhereQuantityLessThan = service.getAllQuantityLessThan(30);
+        ProductTestData.MATCHER.assertCollectionEquals(
+                Arrays.asList(SHELLAC_MANETTI, POTAL_KITAJ), productsWhereQuantityLessThan);
+    }
+}
---
+++ src/test/java/com/malikov/shopsystem/service/AbstractUserServiceTest.java
@@ -0,0 +1,60 @@
+package com.malikov.shopsystem.service;
+
+import com.malikov.shopsystem.model.User;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static com.malikov.shopsystem.UserTestData.*;
+import static com.malikov.shopsystem.model.Role.ROLE_USER;
+
+public abstract class AbstractUserServiceTest extends AbstractServiceTest {
+
+    @Autowired
+    protected UserService service;
+
+    @Test
+    public void testSave() throws Exception {
+        User newUser = new User("newUserNamer", "newPassword", ROLE_USER);
+        User created = service.save(newUser);
+        newUser.setId(created.getId());
+        MATCHER.assertCollectionEquals(
+                Arrays.asList(USER, ADMIN, newUser),
+                service.getAll());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        User updated = new User(USER);
+        updated.setName("User_Updated");
+        service.update(updated);
+        MATCHER.assertEquals(updated, service.get(USER.getId()));
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        User user = service.get(ADMIN.getId());
+        MATCHER.assertEquals(ADMIN, user);
+    }
+
+    @Test
+    public void testGetByName() throws Exception {
+        User admin = service.getByLogin(ADMIN.getName());
+        MATCHER.assertEquals(ADMIN, admin);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        Collection<User> all = service.getAll();
+        MATCHER.assertCollectionEquals(Arrays.asList(USER, ADMIN), all);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        service.delete(USER.getId());
+        MATCHER.assertCollectionEquals(Collections.singletonList(ADMIN), service.getAll());
+    }
+}
