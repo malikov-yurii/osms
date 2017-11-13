47505c805e325e2227823f2d2c4868e516f14114
Malikov <ryki.admirala@gmail.com>
2017-11-13 22:15:56 +0200

    gitignore change; rm .js files

--- src/main/src/app/services/index.js
+++
@@ -1,7 +0,0 @@
-export { ApiService } from './api.service';
-export { OrderService } from './orders.service';
-export { StoreHelper } from './store-helper.service';
-export { SearchService } from './search.service';
-export { ProductService } from './products.service';
-export { CustomerService } from './customers.service';
-export { NotyService } from './noty.service';
