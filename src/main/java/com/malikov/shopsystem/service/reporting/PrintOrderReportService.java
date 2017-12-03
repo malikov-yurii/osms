ec7e0d4ed419c07438f9866c05fd25d1d44dedbb
Yurii Malikov <malikov.yurii.alex@gmail.com>
2017-12-03 12:26:41 +0300

    add customer info to print order template

--- src/main/java/com/malikov/shopsystem/service/reporting/PrintOrderReportService.java
+++ src/main/java/com/malikov/shopsystem/service/reporting/PrintOrderReportService.java
@@ -6,6 +6,7 @@ import com.malikov.shopsystem.model.OrderLine;
 import com.malikov.shopsystem.repository.OrderRepository;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ import java.util.Map;
 
 import static java.util.Collections.emptyList;
 import static java.util.Objects.nonNull;
+import static java.util.Optional.ofNullable;
 import static net.sf.jasperreports.engine.JasperCompileManager.compileReport;
 import static net.sf.jasperreports.engine.JasperExportManager.exportReportToPdf;
 import static net.sf.jasperreports.engine.JasperFillManager.fillReport;
@@ -28,7 +30,12 @@ import static net.sf.jasperreports.engine.JasperFillManager.fillReport;
 @Service
 public class PrintOrderReportService {
 
-    public static final String PRINT_ORDER_TEMPLATE_PATH = "template/printOrder.jrxml";
+    private static final String PRINT_ORDER_TEMPLATE_PATH = "template/printOrder.jrxml";
+    private static final String CUSTOMER_NAME = "customerName";
+    private static final String CUSTOMER_PHONE_NUMBER = "customerPhoneNumber";
+    private static final String DESTINATION = "destination";
+    private static final String TOTAL_ORDER_AMOUNT = "totalOrderAmount";
+    private static final String PAYMENT_TYPE = "paymentType";
 
     @Autowired
     private OrderRepository orderRepository;
@@ -37,12 +44,26 @@ public class PrintOrderReportService {
         Order order = orderRepository.findOne(orderId);
         JRBeanCollectionDataSource source = new JRBeanCollectionDataSource(prepareOrderLines(order));
         String template = getClass().getClassLoader().getResource(PRINT_ORDER_TEMPLATE_PATH).getFile();
-        return exportReportToPdf(fillReport(compileReport(template), parameters(order.getTotalValue()), source));
+        return exportReportToPdf(fillReport(compileReport(template), parameters(order), source));
     }
 
-    private Map<String, Object> parameters(BigDecimal totalOrderAmount) {
+    private Map<String, Object> parameters(Order order) {
         Map<String, Object> parameters = new HashMap<>();
-        parameters.put("totalOrderAmount", totalOrderAmount);
+
+        parameters.put(CUSTOMER_NAME, ofNullable(order.getCustomerFirstName())
+                                        .map(firstName -> order.getCustomerLastName() + " " + firstName)
+                                        .orElse(order.getCustomerLastName()));
+        parameters.put(CUSTOMER_PHONE_NUMBER, ofNullable(order.getCustomerPhoneNumber())
+                                    .filter(StringUtils::isNotBlank)
+                                    .map(phone -> "+38(" + phone.substring(0,3) + ")" + phone.substring(3,5) + "-" +
+                                            phone.substring(5,7) + "-" + phone.substring(7))
+                                    .orElse(""));
+        parameters.put(DESTINATION, ofNullable(order.getDestinationPostOffice())
+                                        .map(postOffice -> order.getDestinationCity() + ", " + postOffice)
+                                        .orElse(order.getDestinationCity()));
+        parameters.put(TOTAL_ORDER_AMOUNT, order.getTotalValue());
+        parameters.put(PAYMENT_TYPE, order.getPaymentType().toString());
+
         return parameters;
     }
 
